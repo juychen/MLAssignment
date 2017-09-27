e23dd1d90a0758f75593d9d7c6b6ca6c49575c1b
junyichen8 <junyichen8-c@my.cityu.edu.hk>
2017-09-27 17:06:19 +0800

    l

---
+++ PA-1/PA1_source_code.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+import math as m
+import matplotlib.pyplot as plt
+
+# cd D:\\OneDrive\\文档\\cityu\\MachineLearning\\MLAssignment\\PA-1\\PA-1-data-text
+
+# define the polynomial function
+def poly_function(x,order = 1):
+    return np.array([m.pow(x,i) for i in range(0,order+1) ])
+
+# load file from txt
+def load_file(filename = 'polydata_data_polyx.txt'):
+    return np.genfromtxt(filename,dtype='double')
+
+# transpose 
+def T(x):
+    if(len(x.shape)>1):
+        return np.transpose(x)
+    else:
+        return x.reshape(1,x.shape[0])
+
+# x is a set of column vectors, get the transpose form of Φ matrix
+def PHIx(x,order=1,function='poly'):
+    if(function == 'poly'):
+        mat = [poly_functionT(item,order) for item in x ]
+        #return np.transpose(np.array(mat))
+        return T(np.array(mat))
+
+# return objective function according to different methods.
+def obj_function(y,PHI,theta,Lambda=0,method='LS'):
+    if method == 'LS':
+        return np.linalg.norm(y-np.dot(T(PHI),theta),ord=2)
+    if method == 'RLS':
+        return np.linalg.norm(y-np.dot(T(PHI),theta),ord=2) + Lambda * np.linalg.norm(theta,ord=2)
+    if method == 'LASSO':
+        return np.linalg.norm(y-np.dot(T(PHI),theta),ord=2) + Lambda * np.linalg.norm(theta,ord=1)
+    if method == 'RR':
+        return np.linalg.norm(y-np.dot(T(PHI),theta),ord=1)
+
+# Generate prediction according to the theta
+def predict(x,theta,function='poly'):
+    if(function=='poly'):
+        PHIX=PHIx(x,order=theta.shape[0]-1,function=function)
+        predections = np.dot(T(PHIX),theta)
+        return predections
+
+# parameter estimate , all input vectors are column vectors
+def para_estimate(y,PHI,Lambda=0,method='LS'):
+    if method == 'LS':
+        return np.dot(np.dot(np.linalg.inv(np.dot(PHI,T(PHI))),PHI),y)
+
+# Generate Plots
+def plot_f_s(x,y,sampx,sampy,label):
+    plt.plot(x, y, label=label)
+    plt.legend()
+    plt.plot(sampx, sampy,'ro',label='data')
+    plt.legend()
+    plt.show()
+    return 
+
+def main():
+    # my code here
+
+if __name__ == "__main__":
+    main()
+
+
+
