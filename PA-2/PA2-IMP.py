dc90ea9b32fec85440e913bf2d3708e2150a43c8
WINGHARE <WINGHARE@GMAIL.COM>
2017-10-22 11:32:40 +0800

    Edit EMGMM

--- PA-2/PA2-IMP.py
+++ PA-2/PA2-IMP.py
@@ -12,6 +12,20 @@ from sklearn.utils import resample
 def gaussian(x,miu,SIGMA):
     return multivariate_normal.pdf(x,miu,SIGMA)
 
+
+    # return the probabilty of each x, dim = N * K
+    # col = probabilty each x.
+    # row = each mixture modelxxx
+def get_mixture_Gaussian_pdf(x,miu,SIGMA):
+    result = np.array([])
+    n = x.shape[0]
+
+    for j in range(0,miu.shape[0]):
+        result= np.append(result,gaussian(x,miu[j],SIGMA[j]))
+        #if j == 0 : n = len(result)
+
+    return result.reshape(-1,n).T
+
 # transpose 
 def T(x):
     if(str(type(x))!="<class 'numpy.ndarray'>"):
@@ -82,27 +96,13 @@ class EMGMM(EMMM):
         return
 
     def initial_SIGMA(self,SIGMA_init=np.array([])):
-        if(SIGMA_init.shape ==(self.d,self.d)):
+        if(SIGMA_init.shape ==(self.K,self.d,self.d)):
             self.SIGMA = SIGMA_init
         else:
             eyed = np.eye(self.d)
-            self.SIGMA = np.array([eyed for i in range(0,K)])
+            self.SIGMA = np.array([eyed for i in range(0,self.K)])
             
         return
-
-    # return the probabilty of each x, dim = N * K
-    # col = probabilty each x.
-    # row = each mixture modelxxx
-    def get_mixture_Gaussian_pdf(x,miu,SIGMA):
-
-        result = np.array([])
-        n = 0
-
-        for j in range(0,miu.shape[0]):
-            result= np.append(result,gaussian(x,miu[j],SIGMA[j]))
-            if j == 0 : n = len(result)
-
-        return result.reshape(miu.shape[0],n).T
 
     def cluster(self):
         if(len(self.x)<1): 
@@ -110,9 +110,14 @@ class EMGMM(EMMM):
             return 
 
         count = 0 
+        N = self.x.shape[0]
+
         while(count<self.itera):
             count +=1
             z = np.array([])
+
+            if count == 3: 
+                print(0)
             
             # return the probabilty of each x, dim = N * K
             NG = get_mixture_Gaussian_pdf(self.x,self.miu,self.SIGMA)
@@ -121,8 +126,29 @@ class EMGMM(EMMM):
             scalar = np.dot(NG , dpi)
             denominator = np.sum(scalar,axis=1)
             z = (scalar.T/denominator).T          
+
+            N_j = np.sum(z,axis=0)
+            pi = N_j/N
+
+            miu = ((np.dot(z.T,self.x).T)/N_j).T   
+
+            SIGMA = [np.dot(((self.x - miu[j]).T * z[:,j]),self.x - miu[j]) for j in range(0,self.K)]
+            SIGMA = np.array(SIGMA)
+
+            if(np.linalg.norm(self.miu-miu)<self.threshold and np.linalg.norm(self.SIGMA-SIGMA)<self.threshold):
+                self.z = z
+                self.miu = miu
+                self.SIGMA = SIGMA
+
+                return 
+
             self.z = z
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+            self.miu = miu
+            self.SIGMA = SIGMA
+        
+            #for j in range(0,self.K):
+                #print(j)
+                #SIGMA_j = np.dot(((self.x - miu[j]).T * z[:,j]),self.x - miu[j])                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 
         return
          
@@ -177,7 +203,12 @@ class Kmeans(ClusterAlgorithm):
 
 def main():
     
-    GMM = EMGMM()
+    GMM = EMGMM(k=3,dimension=2)
+    x = np.array([[1,2],[7,3],[1,4],[5,6]])
+    GMM.fit_x(x)
+    GMM.cluster()
+
+    print (GMM.z)
     return 
 
 
