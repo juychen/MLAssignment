7279868c53ef7381495e3a3b99d2fdd99eb2c4f6
CHEN Junyi <junyichen8@ad.cityu.edu.hk>
2017-10-23 15:40:07 +0800

    Change location of kmeans

--- PA-2/PA2-IMP.py
+++ PA-2/PA2-IMP.py
@@ -6,7 +6,6 @@ import pandas as pd
 from numpy.random import shuffle
 from scipy.spatial.distance import euclidean
 from scipy.stats import multivariate_normal
-
 from sklearn.utils import resample
 
 
@@ -41,6 +40,9 @@ def T(x):
     else:
         return x.reshape(-1, 1)
 
+# In our experiment, input of x is row wise
+# shape of x is N * D while N is the number of data point, D is dimension of each data
+
 
 class ClusterAlgorithm:
     K = 1
@@ -63,120 +65,6 @@ class ClusterAlgorithm:
         self.z = np.zeros((self.N, self.K))
         return
 
-
-class EMMM(ClusterAlgorithm):
-
-    pi = np.array([])
-
-    def fit_x(self, x, pi=[]):
-        ClusterAlgorithm.fit_x(self, x)
-        self.initial_pi(pi)
-        return
-
-    def initial_pi(self, pi_init=[]):
-        if(len(pi_init) == self.K):
-            self.pi = np.array(pi_init)
-            return
-        else:
-            self.pi = np.ones(self.K) / self.K
-            return
-
-
-class EMGMM(EMMM):
-
-    miu = np.array([])
-    SIGMA = np.array([])
-    d = 1
-
-    def __init__(self, k=1, itera=10, threshold=0.005):
-        EMMM.__init__(self, k, itera, threshold)
-        # self.d=dimension
-
-    def fit_x(self, x, miu=[], SIGMA=np.array([])):
-        EMMM.fit_x(self, x)
-        self.d = x.shape[1]
-        self.initial_miu(miu_init=miu)
-        self.initial_SIGMA(SIGMA_init=SIGMA)
-
-        return
-
-    def initial_miu(self, miu_init=[], sample=True):
-        if(len(miu_init) == self.d):
-            self.miu = np.array(miu_init)
-
-        elif(sample is True):
-            self.miu = resample(self.x, n_samples=self.K, replace=False,random_state=self.d)
-        else:
-            avg = np.average(self.x)
-            inivalues = np.linspace(avg, self.K, num=(self.K) * self.d)
-            self.miu = inivalues.reshape(self.K, self.d)
-        return
-
-    def initial_SIGMA(self, SIGMA_init=np.array([])):
-
-        # you can assign any values to the initial sigma or just use eye matrix
-        if(SIGMA_init.shape == (self.K, self.d, self.d)):
-            self.SIGMA = SIGMA_init
-        else:
-            eyed = np.eye(self.d)
-            self.SIGMA = np.array([eyed for i in range(0, self.K)])
-
-        return
-
-    def cluster(self):
-        if(len(self.x) < 1):
-            print('no data')
-            return
-
-        count = 0
-        N = self.x.shape[0]
-
-        while(count < self.itera):
-            count += 1
-            z = np.array([])
-
-            if count == 3:
-                print(0)
-
-            # return the probabilty of each x, dim = N * K
-            # sample gaussian outputs a martix of probability of each components of all samples dim =  N (samples) * K (components)
-            sample_gaussians = get_mixture_Gaussian_pdf(
-                self.x, self.miu, self.SIGMA)
-            dpi = np.diag(self.pi)
-
-            # E step z_ij = pi_j * Gaussian(x_i,theta_j)/ sum_over_k(pi_k * Gaussian(x_i,theta_k))
-            z_numerator = np.dot(sample_gaussians, dpi)
-            z_denominator = np.sum(z_numerator, axis=1)
-            z = (z_numerator.T / z_denominator).T
-
-            # M step
-            N_j = np.sum(z, axis=0)
-            pi = N_j / N
-
-            miu = ((np.dot(z.T, self.x).T) / N_j).T
-
-            SIGMA = [np.dot(((self.x - miu[j]).T * z[:, j]), self.x - miu[j])
-                     for j in range(0, self.K)]
-            SIGMA = np.array(SIGMA)
-
-            # Temination of iteration
-            if((np.linalg.norm(self.miu - miu) < self.threshold) and (np.linalg.norm(self.SIGMA - SIGMA) < self.threshold)):
-                self.z = z
-                self.miu = miu
-                self.SIGMA = SIGMA
-                print(count)
-                return
-
-            self.z = z
-            self.miu = miu
-            self.SIGMA = SIGMA
-
-            # for j in range(0,self.K):
-            # print(j)
-            #SIGMA_j = np.dot(((self.x - miu[j]).T * z[:,j]),self.x - miu[j])
-        return
-
-
 class Kmeans(ClusterAlgorithm):
 
     miu = np.array([])
@@ -186,7 +74,8 @@ class Kmeans(ClusterAlgorithm):
             self.miu = self.x[pick_index]
             return
         else:
-            self.miu = resample(self.x, n_samples=self.K, replace=False)
+            self.miu = resample(self.x, n_samples=self.K,
+                                replace=False, random_state=self.K)
             return
 
     def fit_x(self, x, pick=[]):
@@ -230,18 +119,122 @@ class Kmeans(ClusterAlgorithm):
                 self.miu = tmiu
         return
 
+class EMMM(ClusterAlgorithm):
+
+    pi = np.array([])
+
+    def fit_x(self, x, pi=[]):
+        ClusterAlgorithm.fit_x(self, x)
+        self.initial_pi(pi)
+        return
+
+    def initial_pi(self, pi_init=[]):
+        if(len(pi_init) == self.K):
+            self.pi = np.array(pi_init)
+            return
+        else:
+            self.pi = np.ones(self.K) / self.K
+            return
+
+class EMGMM(EMMM):
+
+    miu = np.array([])
+    SIGMA = np.array([])
+    d = 1
+
+    def __init__(self, k=1, itera=10, threshold=0.005):
+        EMMM.__init__(self, k, itera, threshold)
+        # self.d=dimension
+
+    def fit_x(self, x, miu=[], SIGMA=np.array([])):
+        EMMM.fit_x(self, x)
+        self.d = x.shape[1]
+        self.initial_miu(miu_init=miu)
+        self.initial_SIGMA(SIGMA_init=SIGMA)
+
+        return
+
+    def initial_miu(self, miu_init=[], sample=True):
+        if(len(miu_init) == self.d):
+            self.miu = np.array(miu_init)
+
+        elif(sample is True):
+            self.miu = resample(self.x, n_samples=self.K,
+                                replace=False, random_state=self.d)
+        else:
+            avg = np.average(self.x)
+            inivalues = np.linspace(avg, self.K, num=(self.K) * self.d)
+            self.miu = inivalues.reshape(self.K, self.d)
+        return
+
+    def initial_SIGMA(self, SIGMA_init=np.array([])):
+
+        # you can assign any values to the initial sigma or just use eye matrix
+        if(SIGMA_init.shape == (self.K, self.d, self.d)):
+            self.SIGMA = SIGMA_init
+        else:
+            eyed = np.eye(self.d)
+            self.SIGMA = np.array([eyed for i in range(0, self.K)])
+
+        return
+
+    def cluster(self):
+        if(len(self.x) < 1):
+            print('no data')
+            return
+
+        count = 0
+        N = self.x.shape[0]
+
+        while(count < self.itera):
+            count += 1
+            z = np.array([])
+
+            # return the probabilty of each x, dim = N * K
+            # sample gaussian outputs a martix of probability of each components of all samples dim =  N (samples) * K (components)
+            sample_gaussians = get_mixture_Gaussian_pdf(
+                self.x, self.miu, self.SIGMA)
+            dpi = np.diag(self.pi)
+
+            # E step z_ij = pi_j * Gaussian(x_i,theta_j)/ sum_over_k(pi_k * Gaussian(x_i,theta_k))
+            z_numerator = np.dot(sample_gaussians, dpi)
+            z_denominator = np.sum(z_numerator, axis=1)
+            z = (z_numerator.T / z_denominator).T
+
+            # M step
+            N_j = np.sum(z, axis=0)
+            pi = N_j / N
+
+            miu = ((np.dot(z.T, self.x).T) / N_j).T
+
+            SIGMA = [np.dot(((self.x - miu[j]).T * z[:, j]), self.x - miu[j])
+                     for j in range(0, self.K)]
+            SIGMA = np.array(SIGMA)
+
+            # Temination of iteration
+            if((np.linalg.norm(self.miu - miu) < self.threshold) and (np.linalg.norm(self.SIGMA - SIGMA) < self.threshold)):
+                self.z = z
+                self.miu = miu
+                self.SIGMA = SIGMA
+                print(count)
+                return
+
+            self.z = z
+            self.miu = miu
+            self.SIGMA = SIGMA
+        return
+
 
 def main():
 
     GMM = EMGMM(k=3, itera=100)
-    x = np.array([[1, 1], [2, 2], [9, 7], [15, 15], [140, 140]])
+    x = np.array([[1, 1], [2, 2], [9, 7], [15, 15], [105, 5]])
     GMM.fit_x(x)
-    print(GMM.d)
     GMM.cluster()
 
     print(GMM.z)
-
-    print(np.sum(GMM.pi))
+    print(GMM.miu)
+    # print(GMM.SIGMA)
     return
 
 
