bbf748b46025736e488b285bc654ca8d45e62d26
CHEN Junyi <junyichen8@ad.cityu.edu.hk>
2017-11-02 17:25:47 +0800

    ADD 4d weight kmeans

--- PA-2/PA2_IMP.py
+++ PA-2/PA2_IMP.py
@@ -135,6 +135,73 @@ class Kmeans(ClusterAlgorithm):
         return
 
 
+class WeightedKmeans4D(Kmeans):
+    """Weighted Kmeans allow weighting between feature typs"""
+    Lambda = 0
+
+    def __init__(self, k=1, itera=100, threshold=0.005, Lambda=0.1):
+        ClusterAlgorithm.__init__(self, k, itera, threshold)
+        self.Lambda = Lambda
+
+    def weighted_distance_4d(self, a, b):
+        """
+        - compute weighted distance
+        - a is a metrix n * 4
+        - b is a vector 1 * 4
+        - retutn a distance vector 1 * 4
+        """
+        if(self.d != 4):
+            print('it is not a 4 - dimensional data clustering')
+            return 
+
+        minus = a - b
+
+        left = np.square(np.linalg.norm(minus[:,:2],axis=1)) 
+        right = np.square(np.linalg.norm(minus[:,2:],axis=1)) 
+
+        return left + right
+
+    def cluster(self):
+        """start the clustering procedure"""
+        if(len(self.x) < 1):
+            print('no data')
+            return
+        
+        if(self.d != 4):
+            print('it is not a 4 - dimensional data clustering')
+            return 
+
+        count = 0
+
+        while(count < self.itera):
+
+            # Cluster Assignment
+            z = np.array([])
+            count += 1
+            distance = np.array(
+                [self.weighted_distance_4d(self.x,item) for item in self.miu]).T
+
+            for item in distance:
+                midx = np.argmin(item)
+                item = 0 * item
+                item[midx] = 1
+                z = np.append(z, item)
+
+            self.z = z.reshape(distance.shape)
+
+            # Estimate center
+            tmiu = (np.dot(self.z.T, self.x)) / np.sum(self.z, axis=0)[:, None]
+
+            # Termination of iteration
+            if(np.linalg.norm(self.miu - tmiu) < self.threshold):
+                self.miu = tmiu
+                return
+
+            else:
+                self.miu = tmiu
+        return
+
+
 class EMMM(ClusterAlgorithm):
     """Base class for all EM clustering mixture model"""
     pi = np.array([])
@@ -234,56 +301,55 @@ class EMGMM(EMMM):
 
             # SIMGA = 1/N[j] * sum_over_i(z[i][j]*(x[i]-miu[j])(x[i]-miu[j]).T)
             SIGMA = np.array([np.dot(((self.x - miu[j]).T * z[:, j]), self.x - miu[j])
-                     for j in range(0, self.K)])
-            SIGMA = (SIGMA.T/N_j).T
-
+                              for j in range(0, self.K)])
+            SIGMA = (SIGMA.T / N_j).T
 
             # Temination of iteration
             if((np.linalg.norm(self.miu - miu) < self.threshold) and (np.linalg.norm(self.SIGMA - SIGMA) < self.threshold)):
-                self.z=z
-                self.miu=miu
-                self.SIGMA=SIGMA
-                self.pi=pi
+                self.z = z
+                self.miu = miu
+                self.SIGMA = SIGMA
+                self.pi = pi
                 print(count)
                 return
 
-            self.z=z
-            self.miu=miu
-            self.SIGMA=SIGMA
-            self.pi=pi
+            self.z = z
+            self.miu = miu
+            self.SIGMA = SIGMA
+            self.pi = pi
         return
 
 
 class GaussianMeanShift(ClusterAlgorithm):
     """MeanShift clustering algorithm"""
-    h=0
-    x_=np.array([])
-    kernel=''
+    h = 0
+    x_ = np.array([])
+    kernel = ''
     # Bandiwith of the kernel
 
-    def __init__(self, itera = 10, threshold = 0.005, bandwidth = 5, kernel = 'gaussian'):
-        ClusterAlgorithm.__init__(self, itera = itera, threshold = threshold)
-        self.kernel=kernel
-        self.h=bandwidth
+    def __init__(self, itera=10, threshold=0.005, bandwidth=5, kernel='gaussian'):
+        ClusterAlgorithm.__init__(self, itera=itera, threshold=threshold)
+        self.kernel = kernel
+        self.h = bandwidth
 
     def fit_x(self, x):
         ClusterAlgorithm.fit_x(self, x)
-        self.x_=x
+        self.x_ = x
         return
 
     def update(self):
         if(self.kernel == 'gaussian'):
-            dia_sqrh=np.eye(self.d) * (self.h) * (self.h)
-            covs=np.array([dia_sqrh for i in range(0, self.N)])
+            dia_sqrh = np.eye(self.d) * (self.h) * (self.h)
+            covs = np.array([dia_sqrh for i in range(0, self.N)])
 
             # return the probabilty of each x, dim = N * N
             # sample gaussian outputs a martix of probability of each components of all samples dim =  N (samples) * N (components)
-            sample_gaussians=get_mixture_Gaussian_pdf(
+            sample_gaussians = get_mixture_Gaussian_pdf(
                 self.x, self.x_, covs)
 
-            x_numerator=np.dot(sample_gaussians.T, self.x)
-            x_denominator=np.sum(sample_gaussians, axis = 0)
-            x=(x_numerator.T / x_denominator).T
+            x_numerator = np.dot(sample_gaussians.T, self.x)
+            x_denominator = np.sum(sample_gaussians, axis=0)
+            x = (x_numerator.T / x_denominator).T
 
             return x
 
@@ -291,22 +357,28 @@ class GaussianMeanShift(ClusterAlgorithm):
         if(len(self.x) < 1):
             print('no data')
             return
-        count=0
+        count = 0
         while (count < self.itera):
             count += 1
-            x_=self.update()
+            x_ = self.update()
             if((np.linalg.norm(self.x_ - x_) < self.threshold)):
                 print(count)
-                self.x_=x_
+                self.x_ = x_
                 return
-            self.x_=x_
+            self.x_ = x_
         return
 
 
 def main():
 
-    GMM=EMGMM(k = 3, itera = 100)
-    x=np.array([[1, 1], [2, 2], [9, 7], [15, 15], [105, 5]])
+    KM = WeightedKmeans4D(k=2, itera=100)
+    x = np.array([[1, 1, 2, 2], [9, 7, 15, 15], [1,2,3,4]])
+    KM.fit_x(x)
+    KM.cluster()
+    print(KM.z)
+
+    GMM = EMGMM(k=3, itera=100)
+    x = np.array([[1, 1], [2, 2], [9, 7], [15, 15], [105, 5]])
     GMM.fit_x(x)
     GMM.cluster()
 
@@ -314,7 +386,7 @@ def main():
     print(GMM.miu)
     # print(GMM.SIGMA)
 
-    MS=GaussianMeanShift(bandwidth = 10, itera = 100)
+    MS = GaussianMeanShift(bandwidth=10, itera=100)
     MS.fit_x(x)
     MS.cluster()
     print(MS.x_)
@@ -323,4 +395,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
